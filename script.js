b1d50f1ca6fd21f00959762adaba0bb2fed9edd8
FIKY 0911 <fiky0911@gmail.com>
2025-08-14 22:50:49 +0700

    Footer

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-const profilePic = document.getElementById('profilePic');
-const profileImage = document.getElementById('profileImage');
-
-profilePic.addEventListener('change', () => {
-    profileImage.src = URL.createObjectURL(profilePic.files[0]);
+document.getElementById('menu-toggle').addEventListener('click', function() {
+    const menu = document.getElementById('navbar-menu');
+    menu.classList.toggle('hidden');
+    menu.classList.toggle('flex');
 });
